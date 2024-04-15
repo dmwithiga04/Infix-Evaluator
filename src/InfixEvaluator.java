ecd0bf5336bf407985037ebd8bd8a680769c4f51
Davine Mungai <dmwithiga04@gmail.com>
2024-04-15 00:06:51 -0700

    completed main implementation of Evaluation method

--- src/InfixEvaluator.java
+++ src/InfixEvaluator.java
@@ -10,7 +10,7 @@ public class InfixEvaluator {
     /**
      * Class to report a syntax error.
      */
-    public class SyntaxErrorException extends RuntimeException {
+    public static class SyntaxErrorException extends RuntimeException {
 
         /**
          * Construct a SyntaxErrorException with the specified
@@ -29,24 +29,84 @@ public class InfixEvaluator {
      * @param op the character to be checked
      * @return true if the character is an operator, false otherwise
      */
-    private boolean isOperator(String token) {
+    private static boolean isOperator(String token) {
 
         return token.equals("+") || token.equals("-") || token.equals("*")
-                || token.equals("/") || token.equals("^");
+                || token.equals("/") || token.equals("^") || token.equals("%") || token.equals("(") || token.equals(")");
     }
 
-    private int precedence (String token) {
-        //TODO: finish this off
-        switch (token) {
-            case "/":
-                return 1;
-                break;
-            
+    /**
+     * checks precidence of the current operator
+     * 
+     * @param operator String representation of the operator
+     * @return precedence of operator
+     */
+    private static int precedence(String operator) {
+
+        switch (operator) {
             case "^":
+                return 3;
+            case "/":
+            case "*":
+            case "%":
                 return 2;
-                break;
+            case "+":
+            case "-":
+                return 1;
+            default:
+                return 0;
         }
 
+    }
+
+    /**
+     * Calculates the result of applying the specified operator to the given
+     * operands.
+     *
+     * @param lHS      the left-hand side operand
+     * @param rHS      the right-hand side operand
+     * @param operator the operator to apply
+     * @return the result of the operation
+     */
+    private static int calculateDigits(int lHS, int rHS, String operator) {
+        int result = 0;
+
+        // calculate ^
+        if (operator.equals("^")) {
+            result = 1;
+
+            for (int i = 0; i < rHS; i++) {
+                result *= lHS;
+            }
+            
+        }
+
+        // calculate /
+        if (operator.equals("/")) {
+            result = lHS / rHS;
+        }
+
+        // calculate *
+        if (operator.equals("*")) {
+            result = lHS * rHS;
+        }
+
+        // calculate +
+        if (operator.equals("+")) {
+            result = lHS + rHS;
+        }
+
+        // calculate -
+        if (operator.equals("-")) {
+            result = lHS - rHS;
+        }
+
+        // calculate %
+        if (operator.equals("%")){
+            result = lHS % rHS;
+        }
+
+        return result;
     }
 
     /**
@@ -60,27 +120,61 @@ public class InfixEvaluator {
         // stacks to contain operands and operators
         Stack<Integer> operandStack = new Stack<>();
         Stack<String> operatorStack = new Stack<>();
-        // contain result of processing the expression
-        int result = 0;
 
         // scanner to read expression space by space
         Scanner scanner = new Scanner(expression);
         scanner.useDelimiter(" ");
 
-        //push 1st operand and operator to stack
-        operandStack.push(Integer.valueOf(scanner.next()));
-        operatorStack.push(scanner.next());
-
-        while (scanner.hasNextLine()) {
-            // contsins current token
+        while (scanner.hasNext()) {
             String token = scanner.next();
 
+            if (!isOperator(token)) {
+                operandStack.push(Integer.valueOf(token));
+            } else {
+                if (token.equals("(")) {
+                    operatorStack.push(token); // Push '(' onto the operator stack
+                } 
+                else if (token.equals(")")) {
+                    // Evaluate the expression inside parentheses
+                    while (!operatorStack.isEmpty() && !operatorStack.peek().equals("(")) {
+                        int rhs = operandStack.pop();
+                        int lhs = operandStack.pop();
+                        String op = operatorStack.pop();
+                        operandStack.push(calculateDigits(lhs, rhs, op));
+                    }
+                    // Discard the '('
+                    if (!operatorStack.isEmpty() && operatorStack.peek().equals("(")) {
+                        operatorStack.pop();
+                    } 
+                    else {
+                        // in case of unbalanced parentheses
+                        throw new SyntaxErrorException("Unbalanced parentheses");
+                    }
+                } 
+                else {
+                    // While there are operators on the stack with higher precedence
+                    while (!operatorStack.isEmpty() && precedence(operatorStack.peek()) >= precedence(token)) {
+                        int rhs = operandStack.pop();
+                        int lhs = operandStack.pop();
+                        String op = operatorStack.pop();
+                        operandStack.push(calculateDigits(lhs, rhs, op));
+                    }
+                    // Push the current operator to the operator stack
+                    operatorStack.push(token);
+                }
             }
-
-            
         }
 
-        return result;
+        // Evaluate remaining operations
+        while (!operatorStack.isEmpty()) {
+            int rhs = operandStack.pop();
+            int lhs = operandStack.pop();
+            String op = operatorStack.pop();
+            operandStack.push(calculateDigits(lhs, rhs, op));
+        }
+
+        scanner.close();
+        return operandStack.peek();
     }
 
 }
--- tests/InfixEvaluatorTest.java
+++ tests/InfixEvaluatorTest.java
@@ -18,4 +18,39 @@ public class InfixEvaluatorTest {
         Assert.assertEquals(18, InfixEvaluator.evaluate("2 * 3 ^ 2"));
     }
 
+     @Test
+    public void testExpression04() {
+        Assert.assertEquals(11, InfixEvaluator.evaluate("( 4 + 3 ) * 2 - 3"));
+    }
+
+    @Test
+    public void testExpression05() {
+        Assert.assertEquals(17, InfixEvaluator.evaluate("5 + 3 * ( 6 - 2 )"));
+    }
+
+    @Test
+    public void testExpression06() {
+        Assert.assertEquals(8, InfixEvaluator.evaluate("2 ^ 3"));
+    }
+
+    @Test
+    public void testExpression07() {
+        Assert.assertEquals(64, InfixEvaluator.evaluate("( 2 ^ 3 ) ^ 2"));
+    }
+
+    @Test
+    public void testExpression08() {
+        Assert.assertEquals(10, InfixEvaluator.evaluate("(( 4 * 2 ) + 6 ) / 2"));
+    }
+
+    @Test
+    public void testExpression09() {
+        Assert.assertEquals(9, InfixEvaluator.evaluate("(( 4 * 2 ) + 6 ) / ( 2 + 1 )"));
+    }
+
+    @Test
+    public void testExpression10() {
+        Assert.assertEquals(6, InfixEvaluator.evaluate("( ( 1 + 2 ) * ( 4 - 1 ) ) / 3"));
+    }
+
 }
