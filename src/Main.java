87eb4efc802e1584533ac99fc67dba0db0837659
DavineM <dmwithiga04@gmail.com>
2024-09-26 10:14:47 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        // Example infix expression
+        String expression = "( 3 + 5 ) * 2";
+    
+        // Evaluate the expression using InfixEvaluator
+        int result = InfixEvaluator.evaluate(expression);
+        System.out.println("The result of the expression '" + expression + "' is: " + result);
+    
     }
 }
